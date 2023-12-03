e0a93eea1a2448f9abbeca43dc369a0a996fb165
Miroslav Stastny <miroslav.stastny@microsoft.com>
2023-12-02 22:40:05 -0800

    Add last question feedback

--- src/Exam.tsx
+++ src/Exam.tsx
@@ -15,6 +15,9 @@ export const Exam: React.FunctionComponent<{
 }> = ({ onComplete, totalSec }) => {
   const [question, setQuestion] = React.useState(nextQuestion())
   const [stats, setStats] = React.useState({ correct: 0, wrong: 0 })
+  const [lastAnswer, setLastAnswer] = React.useState<'correct' | 'wrong'>(
+    'correct',
+  )
 
   const handleInput = React.useCallback<
     Required<React.ComponentProps<typeof Input>>['onSubmit']
@@ -22,9 +25,11 @@ export const Exam: React.FunctionComponent<{
     (_, answer) => {
       if (answer === question.answer) {
         setQuestion(nextQuestion())
-        setStats({ ...stats, correct: stats.correct + 1 })
+        setLastAnswer('correct')
+        setStats((stats) => ({ ...stats, correct: stats.correct + 1 }))
       } else {
-        setStats({ ...stats, wrong: stats.wrong + 1 })
+        setLastAnswer('wrong')
+        setStats((stats) => ({ ...stats, wrong: stats.wrong + 1 }))
       }
     },
     [question, setQuestion],
@@ -33,11 +38,14 @@ export const Exam: React.FunctionComponent<{
   return (
     <>
       <Timer totalSec={totalSec} onExpired={() => onComplete(stats)} />
-      <div>{question.question}</div>
+      <h2>{question.question}</h2>
+      <div>
+        {lastAnswer === 'wrong' ? 'Last answer was wrong :-(' : <>&nbsp;</>}
+      </div>
+      <Input onSubmit={handleInput} />
       <div>
         correct: {stats.correct} wrong: {stats.wrong}
       </div>
-      <Input onSubmit={handleInput} />
     </>
   )
 }
